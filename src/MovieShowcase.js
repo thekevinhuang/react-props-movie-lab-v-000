24735d306887f7868bcb158239bd0d81324120a2
Kevin Huang <yellow.khaos@gmail.com>
2019-01-24 02:36:57 +0000

    Automatically backed up by Learn

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -6,7 +6,7 @@ export default class MovieShowcase extends Component {
 
   generateMovieCards = () => {
     // map over your movieData array and return the correct
-    movieData.map((movie) => <MovieCard movies={movie}/> )
+    return movieData.map((movie) => <MovieCard movies={movie}/> )
   }
 
   render() {
